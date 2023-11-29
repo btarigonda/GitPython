4899c4fe21a12cfddc35825b72cd23870dc4c1d2
btarigonda <dev.b.tarigonda@gmail.com>
2023-11-29 18:12:01 -0500

    lab3 changes

---
+++ PythonLab/Lab3.py
@@ -0,0 +1,13 @@
+myString = "This is a string."
+print(myString)
+print(type(myString))
+print(myString + " Is of the data type " + str(type(myString)))
+firstString = "Bala"
+secondString = "Vani"
+thirdString = firstString + secondString
+print(thirdString)
+name = input("What is your name?")
+print (name)
+color = input("What is your favorite color?  ")
+flower = input("What is your favorite flower?  ")
+print("{}, you like a {} {}!!!".format(name,color,flower))
